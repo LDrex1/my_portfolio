080c0bdf4b2a8b262d52775b1398e007661249a7
lDrex1 <damzealwini7@gmail.com>
2022-07-05 19:36:02 +0100

    breakpoints update

--- src/components/Device.js
+++ src/components/Device.js
@@ -1,21 +1,14 @@
-// import React from 'react'
-
-// function Device() {
-//   return (
-//     <div>Device</div>
-//   )
-// }
-
-// export default Device
-
+//
 const size = {
-  mobileM: "380px",
-  mobileL: "1000px",
+  mobileS: "380px",
+  mobileM: "548px",
+  mobileL: "769px",
   laptop: "1023px",
 };
 //
 
 export const device = {
+  mobileS: `(min-width: ${size.mobileS})`,
   mobileM: `(min-width: ${size.mobileM})`,
   mobileL: `(min-width: ${size.mobileL})`,
   laptop: `(min-width: ${size.laptop})`,
