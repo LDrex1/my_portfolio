deca43057fa2c17a3f2d4ee51ed11c74e9929dc0
lDrex1 <damzealwini7@gmail.com>
2022-08-04 00:15:05 +0100

    update: Header and Subheader

--- src/components/ProjectsPage.js
+++ src/components/ProjectsPage.js
@@ -9,6 +9,8 @@ function ProjectsPage() {
   //
   return (
     <Wrapper>
+      <Header>MY PROJECTS</Header>
+      <SubHeader>Click each image to know more</SubHeader>
       <Container>
         {/*  */}
         <Projects />
@@ -21,11 +23,16 @@ function ProjectsPage() {
 export default ProjectsPage;
 
 const Wrapper = styled.section`
-  background: #1a052e;
+  background: #ffce99;
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  align-items: center;
 `;
 //
 
 const Container = styled.div``;
 //
+
+const Header = styled.h2``;
+//
+const SubHeader = styled.h5``;
