2bd84c96f71541d0f4e4c6013f3482260d96b8bb
lDrex1 <damzealwini7@gmail.com>
2022-08-04 02:31:44 +0100

    update: -image for getMyPosts. -grammar corrections

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -16,19 +16,8 @@ const sites = [
       gitHub: "https://github.com/LDrex1/mechanic",
     },
     about:
-      "A multipage website on an automobile repair company. All pages are fully responsive.",
+      "A multipaged website on an automobile repair company based on an existing figma design. All pages are fully responsive.",
     resources: ["HTML", "and CSS"],
-  },
-  {
-    name: "mY-mInD",
-    image: require("../images/cfuf.png"),
-    links: {
-      live: "https://ldrex1.github.io/posts_from_API/",
-      gitHub: "https://github.com/LDrex1/posts_from_API",
-    },
-    about: `A website where you can check, update, delete and view posts, similar to a social media website. Data was gotten from "yourplaceholder API".
-       All pages are fully responsive.`,
-    resources: ["HTML", "CSS", "and JavaScript"],
   },
   {
     name: "CSUF",
@@ -40,6 +29,17 @@ const sites = [
     about: `A tech brand website built based on an existing figma design.
        All pages are fully responsive.`,
     resources: ["HTML", "and Bootstrap"],
+  },
+  {
+    name: "getMyPosts",
+    image: require("../images/getmyposts.png"),
+    links: {
+      live: "https://ldrex1.github.io/posts_from_API/",
+      gitHub: "https://github.com/LDrex1/posts_from_API",
+    },
+    about: `A webApp where you can check, update, delete and view posts, similar to a social media website. Data was gotten from "yourplaceholder API".
+       All pages are fully responsive.`,
+    resources: ["HTML", "CSS", "and JavaScript"],
   },
   {
     name: "Utopia",
