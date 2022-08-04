92129c96e6cbf27552e058b89a29fe541fbd7e0b
lDrex1 <damzealwini7@gmail.com>
2022-08-04 01:39:34 +0100

    update: footer component added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import Abiola from "./components/Abiola";
 import "./App.css";
 import ProjectsPage from "./components/ProjectsPage";
 import ConnectTemp from "./components/ConnectTemp";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Abiola />
       <ProjectsPage />
-      <ConnectTemp />
+      <Footer />
     </>
   );
 }
