a44b707d641301e85f2042ca83a5fa276e2e42a9
lDrex1 <damzealwini7@gmail.com>
2022-08-13 23:18:57 +0100

    update

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -145,8 +145,8 @@ const Mask = styled.div`
   width: 100%;
   mix-blend-mode: multiply;
   visibility: hidden;
-  filter: blur(10px);
-  box-shadow: 0 0 1px rgba(0, 0, 0, 0.05);
+  filter: blur(8px);
+  box-shadow: 0 0 1px rgba(0, 0, 0, 0.6);
   outline: 2px solid transparent;
 `;
 //
