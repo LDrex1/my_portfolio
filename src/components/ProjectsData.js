0c74e0464570289c440cd7aa7e3b21c0728e12a9
lDrex1 <damzealwini7@gmail.com>
2022-09-11 07:06:10 +0100

    new: component for storing project data

---
+++ src/components/ProjectsData.js
@@ -0,0 +1,63 @@
+/**
+ * Data Containing information on all the projects
+ */
+export const sites = [
+  {
+    name: "finsweet",
+    image: require("../images/finsweet.png"),
+    links: {
+      live: "https://ldrex1.github.io/mechanic/",
+      gitHub: "https://github.com/LDrex1/mechanic",
+    },
+    about:
+      "A multipaged website on an automobile repair company based on an existing figma design. All pages are fully responsive.",
+    resources: ["HTML", "and CSS"],
+  },
+  {
+    name: "Quotes",
+    image: require("../images/Quotes.png"),
+    links: {
+      live: "https://ldrex1.github.io/Quotes/",
+      gitHub: "https://github.com/LDrex1/Quotes",
+    },
+    about: `A webApp where you can post your favorite quote of all 
+    time and also see quotes from others. Uses authetication 
+    services from firebase`,
+    resources: ["Reactjs,", "SCSS", "and Firebase"],
+  },
+  {
+    name: "CSUF",
+    image: require("../images/cfuf.png"),
+    links: {
+      live: "https://ldrex1.github.io/CSUF/",
+      gitHub: "https://github.com/LDrex1/CSUF",
+    },
+    about: `A tech brand website built based on an existing figma design.
+         All pages are fully responsive.`,
+    resources: ["HTML", "and Bootstrap"],
+  },
+  {
+    name: "Utopia",
+    image: require("../images/utopia.png"),
+    links: {
+      live: "https://ldrex1.github.io/countriesInfo/",
+      gitHub: "https://github.com/LDrex1/countriesInfo",
+    },
+    about: `A two-paged webApp that shows all the countries of the world and their main data.
+        Data is gotten from the "countries rest API".
+         All pages are fully responsive.`,
+    resources: ["HTML,", "CSS", "and JavaScript"],
+  },
+  {
+    name: "getMyPosts",
+    image: require("../images/getmyposts.png"),
+    links: {
+      live: "https://ldrex1.github.io/posts_from_API/",
+      gitHub: "https://github.com/LDrex1/posts_from_API",
+    },
+    about: `A webApp where you can check, update, delete and view posts, similar to a social media website. Data was gotten from "yourplaceholder API".
+         All pages are fully responsive.`,
+    resources: ["HTML,", "CSS", "and JavaScript"],
+  },
+];
+//
