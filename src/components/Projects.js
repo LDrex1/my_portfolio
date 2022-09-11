1d575ff43445e4fecd3c7191c10fce7d9ea94f22
lDrex1 <damzealwini7@gmail.com>
2022-09-11 07:05:54 +0100

    moving the projects data to a new file

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,58 +2,7 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import { keyframes, css } from "styled-components";
 import { device } from "./Device";
-//
-
-/**
- * Data Containing information on all the projects
- */
-const sites = [
-  {
-    name: "finsweet",
-    image: require("../images/finsweet.png"),
-    links: {
-      live: "https://ldrex1.github.io/mechanic/",
-      gitHub: "https://github.com/LDrex1/mechanic",
-    },
-    about:
-      "A multipaged website on an automobile repair company based on an existing figma design. All pages are fully responsive.",
-    resources: ["HTML", "and CSS"],
-  },
-  {
-    name: "CSUF",
-    image: require("../images/cfuf.png"),
-    links: {
-      live: "https://ldrex1.github.io/CSUF/",
-      gitHub: "https://github.com/LDrex1/CSUF",
-    },
-    about: `A tech brand website built based on an existing figma design.
-       All pages are fully responsive.`,
-    resources: ["HTML", "and Bootstrap"],
-  },
-  {
-    name: "getMyPosts",
-    image: require("../images/getmyposts.png"),
-    links: {
-      live: "https://ldrex1.github.io/posts_from_API/",
-      gitHub: "https://github.com/LDrex1/posts_from_API",
-    },
-    about: `A webApp where you can check, update, delete and view posts, similar to a social media website. Data was gotten from "yourplaceholder API".
-       All pages are fully responsive.`,
-    resources: ["HTML", "CSS", "and JavaScript"],
-  },
-  {
-    name: "Utopia",
-    image: require("../images/utopia.png"),
-    links: {
-      live: "https://ldrex1.github.io/countriesInfo/",
-      gitHub: "https://github.com/LDrex1/countriesInfo",
-    },
-    about: `A two-paged webApp that shows all the countries of the world and their main data.
-      Data is gotten from the "countries rest API".
-       All pages are fully responsive.`,
-    resources: ["HTML", "CSS", "and JavaScript"],
-  },
-];
+import { sites } from "./ProjectsData";
 //
 
 /**
