a9bf3e2963de59a089268750cc05de8ec7b5e36d
lDrex1 <damzealwini7@gmail.com>
2022-12-17 14:38:46 +0100

    added: kimberly project

--- src/components/ProjectsData.js
+++ src/components/ProjectsData.js
@@ -3,15 +3,17 @@
  */
 export const sites = [
   {
-    name: "finsweet",
-    image: require("../images/finsweet.png"),
+    name: "Kimberly Hotels",
+    image: require("../images/kimberly_hms.png"),
     links: {
-      live: "https://ldrex1.github.io/mechanic/",
-      gitHub: "https://github.com/LDrex1/mechanic",
+      live: "https://ldrex1.github.io/HMS/",
+      gitHub: "https://github.com/LDrex1/HMSF",
     },
-    about:
-      "A multipaged website on an automobile repair company based on an existing figma design. All pages are fully responsive.",
-    resources: ["HTML", "and CSS"],
+    about: `A hotel management system website created by a team of web developers. I single-handedly 
+            implemented the landing-page, reservation-bookings, reservation-summary, and card-input
+            pages. I also worked with the team on some of the other pages. 
+            All pages are fully responsive.`,
+    resources: ["HTML", "Bootstrap", "and Javascript"],
   },
   {
     name: "Quotes",
@@ -21,9 +23,20 @@ export const sites = [
       gitHub: "https://github.com/LDrex1/Quotes",
     },
     about: `A webApp where you can post your favorite quote of all 
-    time and also see quotes from others. Uses authetication 
-    services from firebase`,
+            time and also see quotes from others. Uses authetication 
+            services from firebase`,
     resources: ["Reactjs,", "SCSS", "and Firebase"],
+  },
+  {
+    name: "finsweet",
+    image: require("../images/finsweet.png"),
+    links: {
+      live: "https://ldrex1.github.io/mechanic/",
+      gitHub: "https://github.com/LDrex1/mechanic",
+    },
+    about:
+      "A multipaged website on an automobile repair company based on an existing figma design. All pages are fully responsive.",
+    resources: ["HTML", "and CSS"],
   },
   {
     name: "CSUF",
