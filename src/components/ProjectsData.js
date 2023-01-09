895d1483703dab73db76d316d7bb915ff4e8a3f8
lDrex1 <damzealwini7@gmail.com>
2023-01-09 11:28:40 +0100

    update: new project and an array of all projects

--- src/components/ProjectsData.js
+++ src/components/ProjectsData.js
@@ -50,6 +50,22 @@ export const sites = [
     resources: ["HTML", "and Bootstrap"],
   },
   {
+    name: "3D World",
+    image: require("../images/canvas.png"),
+    links: {
+      live: "https://ldrex1.github.io/rhtyhm/",
+      gitHub: "https://github.com/LDrex1/rhythm",
+    },
+    about: `A webApp where you can interact with a 3D canvas.`,
+    resources: [
+      "React Js,",
+      "Material UI",
+      "Three Js",
+      "React-three-fiber",
+      "React-drei",
+    ],
+  },
+  {
     name: "Utopia",
     image: require("../images/utopia.png"),
     links: {
@@ -61,6 +77,68 @@ export const sites = [
          All pages are fully responsive.`,
     resources: ["HTML,", "CSS", "and JavaScript"],
   },
+];
+
+const allProjects = [
+  {
+    name: "Kimberly Hotels",
+    image: require("../images/kimberly_hms.png"),
+    links: {
+      live: "https://ldrex1.github.io/HMS/",
+      gitHub: "https://github.com/LDrex1/HMSF",
+    },
+    about: `A hotel management system website created by a team of web developers. I single-handedly 
+              implemented the landing-page, reservation-bookings, reservation-summary, and card-input
+              pages. I also worked with the team on some of the other pages. 
+              All pages are fully responsive.`,
+    resources: ["HTML", "Bootstrap", "and Javascript"],
+  },
+  {
+    name: "Quotes",
+    image: require("../images/Quotes.png"),
+    links: {
+      live: "https://ldrex1.github.io/Quotes/",
+      gitHub: "https://github.com/LDrex1/Quotes",
+    },
+    about: `A webApp where you can post your favorite quote of all 
+              time and also see quotes from others. Uses authetication 
+              services from firebase`,
+    resources: ["Reactjs,", "SCSS", "and Firebase"],
+  },
+  {
+    name: "finsweet",
+    image: require("../images/finsweet.png"),
+    links: {
+      live: "https://ldrex1.github.io/mechanic/",
+      gitHub: "https://github.com/LDrex1/mechanic",
+    },
+    about:
+      "A multipaged website on an automobile repair company based on an existing figma design. All pages are fully responsive.",
+    resources: ["HTML", "and CSS"],
+  },
+  {
+    name: "CSUF",
+    image: require("../images/cfuf.png"),
+    links: {
+      live: "https://ldrex1.github.io/CSUF/",
+      gitHub: "https://github.com/LDrex1/CSUF",
+    },
+    about: `A tech brand website built based on an existing figma design.
+           All pages are fully responsive.`,
+    resources: ["HTML", "and Bootstrap"],
+  },
+  {
+    name: "Utopia",
+    image: require("../images/utopia.png"),
+    links: {
+      live: "https://ldrex1.github.io/countriesInfo/",
+      gitHub: "https://github.com/LDrex1/countriesInfo",
+    },
+    about: `A two-paged webApp that shows all the countries of the world and their main data.
+          Data is gotten from the "countries rest API".
+           All pages are fully responsive.`,
+    resources: ["HTML,", "CSS", "and JavaScript"],
+  },
   {
     name: "getMyPosts",
     image: require("../images/getmyposts.png"),
@@ -69,7 +147,7 @@ export const sites = [
       gitHub: "https://github.com/LDrex1/posts_from_API",
     },
     about: `A webApp where you can check, update, delete and view posts, similar to a social media website. Data was gotten from "yourplaceholder API".
-         All pages are fully responsive.`,
+           All pages are fully responsive.`,
     resources: ["HTML,", "CSS", "and JavaScript"],
   },
 ];
